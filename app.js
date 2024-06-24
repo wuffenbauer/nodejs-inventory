1c3ad030da29818a022643edc106cf2074c498e9
wuffenbauer <wuffenbauer@gmail.com>
2024-06-24 20:28:31 +0700

    setting aplikasi & route awal

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.set('views', './view-html')
+
+app.get('/', c_beranda.index)
+
+app.listen(port, () => {
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
+
