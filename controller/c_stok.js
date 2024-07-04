689c812064583313dc7164314e889e3ff35c8048
wuffenbauer <wuffenbauer@gmail.com>
2024-07-04 22:05:11 +0700

    stok masuk: perbaikan logic stok sisa

--- controller/c_stok.js
+++ controller/c_stok.js
@@ -4,19 +4,28 @@ const m_stok   = require('../model/m_stok')
 module.exports = {
     form_stok_masuk: async (req, res) => {
         let dataview = {
-            konten      : 'stok/form-stok-masuk',
             req         : req,
+            konten      : 'stok/form-stok-masuk',
             uri_segment : req.path.split('/'),
             info_error  : null,
-            produk      : await m_stok.form_stok_masuk(req)
+            produk      : await m_produk.get_semua_produk()
         }
         res.render('template/struktur', dataview)
     },
 
     proses_stok_masuk: async (req, res) => {
-        try {      
-            let insert      = await m_stok.input_stok_masuk(req)
-            let isi_notif   = `Berhasil input stok masuk`  
+        try {
+            let stok_terakhir = await m_stok.ambil_stoksisa_terakhir(req.body.form_produk) // cek sisa di database
+            let sisa_terakhir = 0
+            
+            if (stok_terakhir.length > 0) {
+                sisa_terakhir = stok_terakhir[0].stok_sisa // ambil objek stok_sisa
+            }
+
+            let hasil_akhir   = sisa_terakhir + Number(req.body.form_jumlah) // jumlahkan dengan stok masuk yang diinput
+            
+            let insert        = await m_stok.input_stok_masuk(req, hasil_akhir)
+            let isi_notif     = `Berhasil input stok masuk`  
             if (insert.affectedRows > 0) {
                 res.redirect(`/stok-masuk?status=sukses&pesan=${isi_notif}`)
             }        
@@ -27,6 +36,7 @@ module.exports = {
                 req         : req,
                 uri_segment : req.path.split('/'),
                 info_error  : error,
+                produk      : await m_produk.get_semua_produk()
             }
             res.render('template/struktur', dataview)
         }
--- model/m_stok.js
+++ model/m_stok.js
@@ -4,37 +4,54 @@ const moment        = require('moment')
 moment.locale('id')
 
 module.exports = {
-    input_stok_masuk: {
-        function(req) {
-            let masuk = req.body.form_jumlah
-            let sisa  = masuk
-            let data  = {
-                kode_produk     : req.body.form_produk,
-                stok_masuk      : req.body.form_jumlah,
-                stok_keluar     : 0,
-                stok_sisa       : sisa,
-                keterangan      : req.body.keterangan,
-                dibuat_oleh     : req.session.user.id,
-                dibuat_kapan    : moment().format('YYYY-MM-DD HH:mm:ss')
-            }
-    
-            let sql = mysql.format(
-                `INSERT INTO stok SET ?`,
-                [data]            
-            )
-    
-            return new Promise((resolve, reject) => {
-                db.query(sql, function(errorSql, hasil) {
-                    if (errorSql) {
-                        reject(errorSql)
-                    }
-                    else {
-                        resolve(hasil)
-                    }
-                })
+    ambil_stoksisa_terakhir: (kode_produk) => {
+        let sql = mysql.format(
+            `SELECT * FROM stok
+            WHERE kode_produk = ?
+            ORDER BY id DESC
+            LIMIT 1`,
+            [kode_produk]            
+        )
+
+        return new Promise((resolve, reject) => {
+            db.query(sql, function(errorSql, hasil) {
+                if (errorSql) {
+                    reject(errorSql)
+                }
+                else {
+                    resolve(hasil)
+                }
             })
-        }
+        })
     },
 
+    input_stok_masuk: function(req, hasil_akhir) {
+        let masuk = req.body.form_jumlah
+        let data  = {
+            kode_produk     : req.body.form_produk,
+            stok_masuk      : req.body.form_jumlah,
+            stok_keluar     : 0,
+            stok_sisa       : hasil_akhir,
+            keterangan      : req.body.form_keterangan,
+            dibuat_oleh     : req.session.user.id,
+            dibuat_kapan    : moment().format('YYYY-MM-DD HH:mm:ss')
+        }
+    
+        let sql = mysql.format(
+            `INSERT INTO stok SET ?`,
+            [data]            
+        )
+    
+        return new Promise((resolve,reject)=>{
+            db.query(sql, function(errorSql, hasil) {
+                if (errorSql) {
+                    reject(errorSql)
+                } else {
+                    resolve(hasil)
+                }
+            })
+        })
+    },
 
+    
 }
